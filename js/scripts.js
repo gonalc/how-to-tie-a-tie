ac686d679a372149844a840b7f0ebbc833d24ad5
gonalc <gonalc8@gmail.com>
2019-02-11 00:10:41 +0100

    arreglo

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,31 @@ $( document ).ready(function() {
       $('.usageExamples ul li').removeClass('active');
       $(this).addClass('active');
     });
+
+
+    // Changing image depending on li actived
+    $('.weddingLink').click(function(){
+      $('.imgExample img').attr('src', 'img/boda_pajarita.jpeg');
+    });
+
+    $('.carWalkLink').click(function(){
+      $('.imgExample img').attr('src', 'img/coche.jpeg');
+    });
+
+    $('.snowLink').click(function(){
+      $('.imgExample img').attr('src', 'img/lady_scarf02.jpeg');
+    });
+
+    $('.kidLink').click(function(){
+      $('.imgExample img').attr('src', 'img/chiquillo_pajarita.jpeg');
+    });
+
+    // Handling Menu
+    $('.menu-button').click(function(){
+      $('.menu').css('left', '0');
+    });
+
+    $('.menu ul li a').click(function(){
+      $('.menu').css('left', '-100vw');
+    });
 });
